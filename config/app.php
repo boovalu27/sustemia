45c99c0c5a28289de8b35e6c1a00142ca2143901
boovalu27 <valeria.pinero@davinci.edu.ar>
2024-12-13 19:27:56 -0300

    cambios changesconfigapp

--- config/app.php
+++ config/app.php
@@ -3,9 +3,9 @@
 return [
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Application Name
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | This value is the name of your application, which will be used when the
     | framework needs to place the application's name in a notification or
@@ -16,9 +16,9 @@ return [
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Application Environment
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
@@ -29,9 +29,9 @@ return [
     'env' => env('APP_ENV', 'production'),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Application Debug Mode
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | When your application is in debug mode, detailed error messages with
     | stack traces will be shown on every error that occurs within your
@@ -42,9 +42,9 @@ return [
     'debug' => (bool) env('APP_DEBUG', false),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Application URL
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | This URL is used by the console to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
@@ -55,9 +55,9 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Application Timezone
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. The timezone
@@ -68,9 +68,9 @@ return [
     'timezone' => 'America/Argentina/Buenos_Aires',
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Application Locale Configuration
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | The application locale determines the default locale that will be used
     | by Laravel's translation / localization methods. This option can be
@@ -85,9 +85,9 @@ return [
     'faker_locale' => env('APP_FAKER_LOCALE', 'es'),
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Encryption Key
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | This key is utilized by Laravel's encryption services and should be set
     | to a random, 32 character string to ensure that all encrypted values
@@ -106,9 +106,9 @@ return [
     ],
 
     /*
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     | Maintenance Mode Driver
-    |----------------------------------------------------------------------
+    |--------------------------------------------------------------------------
     |
     | These configuration options determine the driver used to determine and
     | manage Laravel's "maintenance mode" status. The "cache" driver will
@@ -121,108 +121,6 @@ return [
     'maintenance' => [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
-    ],
-
-    /*
-    |----------------------------------------------------------------------
-    | Service Providers
-    |----------------------------------------------------------------------
-    |
-    | These are the service providers that will be automatically loaded on
-    | the request to your application. You may add your own services to
-    | this array to grant expanded functionality to your application.
-    |
-    */
-
-    'providers' => [
-
-        /*
-         * Laravel Framework Service Providers...
-         */
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        Illuminate\Mail\MailServiceProvider::class,
-        Illuminate\Notifications\NotificationServiceProvider::class,
-        Illuminate\Pagination\PaginationServiceProvider::class,
-        Illuminate\Pipeline\PipelineServiceProvider::class,
-        Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        /*
-         * Only load Collision in non-production environments
-         */
-        env('APP_ENV') !== 'production'
-            ? NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider::class
-            : null,
-
-    ],
-
-    /*
-    |----------------------------------------------------------------------
-    | Class Aliases
-    |----------------------------------------------------------------------
-    |
-    | These are the class aliases that will be registered when this application
-    | is started. You can use these aliases to refer to classes in the application
-    | without needing to write out the full namespace.
-    |
-    */
-
-    'aliases' => [
-        'App' => Illuminate\Support\Facades\App::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Encryption' => Illuminate\Support\Facades\Encryption::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Str' => Illuminate\Support\Facades\Str::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
     ],
 
 ];
