336ef73cdb41f37ff01bd8bfa15c846354f65ed7
boovalu27 <valeria.pinero@davinci.edu.ar>
2024-12-15 14:34:51 -0300

    asset para estilos css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') Administración</title>
-    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-    <link rel="icon" type="image/png" href="{{ url('css/imgs/favicon-32x32.png') }}">
-    <link rel="stylesheet" href="{{ url('css/admin.css') }}"> <!-- Estilos específicos para el admin -->
+    <link rel="icon" type="image/png" href="{{ asset('css/imgs/favicon-32x32.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}"> <!-- Estilos específicos para el admin -->
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="icon" type="image/png" href="{{ url('css/imgs/favicon-32x32.png') }}">
-    <link rel="stylesheet" href="{{ url('css/styles.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('css/imgs/favicon-32x32.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <!-- Bootstrap 5 CSS -->
