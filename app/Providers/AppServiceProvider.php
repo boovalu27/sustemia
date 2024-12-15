90fc809de78dd01c14ac5db9fc5dd9dfefff68e4
boovalu27 <valeria.pinero@davinci.edu.ar>
2024-12-15 15:03:16 -0300

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,26 +1,18 @@
 <?php
 
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot()
     {
-        setlocale(LC_TIME, 'es_ES.UTF-8'); // Asegúrate de que este locale esté disponible en tu servidor
+        // Asegúrate de que este locale esté disponible en tu servidor
+        setlocale(LC_TIME, 'es_ES.UTF-8');
         Carbon::setLocale('es');
+
+        // Forzar el uso de HTTPS en entornos de producción
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
